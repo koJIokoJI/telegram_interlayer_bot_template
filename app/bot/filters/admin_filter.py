a9dcabb875950bbf9454f124be50ecb1db54e48c
mia <agckuu@pau.io>
2025-08-27 16:55:14 +0700

    add logger in admin_filter.py

--- app/bot/filters/admin_filter.py
+++ app/bot/filters/admin_filter.py
@@ -1,9 +1,15 @@
+import logging
+
 from aiogram.filters import BaseFilter
 from aiogram.types import Message
 
 from config.config import settings
 
+logger = logging.getLogger(__name__)
+
 
 class IsAdminFilter(BaseFilter):
     async def __call__(self, message: Message):
-        return message.from_user.id in settings.admin_ids
+        if message.from_user.id in settings.admin_ids:
+            logger.debug("User is owner/admin")
+            return message.from_user.id in settings.admin_ids
